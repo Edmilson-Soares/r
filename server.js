78e79595c94e6a4b032d596291bd8d95bdbc7e9a
Edmilson Soares <69806662+Edmilson-Soares@users.noreply.github.com>
2020-12-28 11:32:27 +0000

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,27 @@ app.use(express.static(__dirname))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-
+var sms = [];
 // Socket 
 const io = require('socket.io')(http)
-
+var cont = 0;
 io.on('connection', (socket) => {
     console.log('Connected...')
+    cont = cont + 1;
+    console.log(cont)
+    socket.broadcast.emit('login', cont)
+    socket.emit('login', cont, sms);
     socket.on('message', (msg) => {
+        sms.push(msg);
         socket.broadcast.emit('message', msg)
     })
+    socket.on('connect', function() {
+        console.log("connected from the client side");
+    });
+    socket.on('disconnect', () => {
+        cont = cont - 1;
+        console.log(cont)
+        socket.broadcast.emit('logout', cont)
+    });
 
-})
+});
